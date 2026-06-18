dc79ce1370fc9abe07d8500aca1cf8d75b039a91
Kailash <kailashee042@gmail.com>
2026-06-18 21:19:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_instance" "first" {
+  ami           = "ami-0521cb2d60cfbb1a6"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
